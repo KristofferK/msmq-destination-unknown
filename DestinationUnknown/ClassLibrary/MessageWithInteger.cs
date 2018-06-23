1693bee4d174bcdbad5c772fe9389202ef94a6e9
KristofferK <kristoffer@minlejebolig.dk>
2018-06-23 17:40:25 +0200

    Add handle response from System B to System A

---
+++ DestinationUnknown/ClassLibrary/MessageWithInteger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class MessageWithInteger
+    {
+        public int Value { get; set; }
+        public string Guid { get; set; }
+        public string ReplyTo { get; set; }
+
+        public MessageWithInteger()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} (guid: {Guid}) (ReplyTo: {ReplyTo}";
+        }
+    }
+}
--- DestinationUnknown/SubsystemA/ProgramA.cs
+++ DestinationUnknown/SubsystemA/ProgramA.cs
@@ -10,12 +10,15 @@ namespace SubsystemA
 {
     class ProgramA
     {
+        private static Random random = new Random();
         static void Main(string[] args)
         {
             Console.Title = "Subsystem A";
 
             var messageFromSubsystemCChannel = MessageQueueGenerator.GenerateMessageQueue(MessageQueueGenerator.CToAChannel);
+            var messageFromSubsystemBChannel = MessageQueueGenerator.GenerateMessageQueue(MessageQueueGenerator.BToAChannel);
             ReceiveMessageFromSubsystemC(messageFromSubsystemCChannel);
+            ReceiveMessageFromSubsystemB(messageFromSubsystemBChannel);
             RequestAdressFromSubsystemC();
 
             Console.ReadLine();
@@ -34,6 +37,7 @@ namespace SubsystemA
             Console.WriteLine("Requesting the address of Subsystem B trhough Subsystem C");
             channel.Send(message);
         }
+
         private static void ReceiveMessageFromSubsystemC(MessageQueue channel)
         {
             channel.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
@@ -51,7 +55,40 @@ namespace SubsystemA
 
         private static void SendMessagesToSubsystemB(string addresss)
         {
-            Console.WriteLine("** Should send four messages to Subsystem B **");
+            var channel = MessageQueueGenerator.GenerateMessageQueue(addresss);
+            var guids = new string[]
+            {
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString()
+            };
+            for (var i = 0; i < 4; i++)
+            {
+                var message = new Message()
+                {
+                    Body = new MessageWithInteger()
+                    {
+                        Value = random.Next(1, 50),
+                        Guid = guids[i / 2],
+                        ReplyTo = MessageQueueGenerator.BToAChannel
+                    }
+                };
+                Console.WriteLine($"Sending message {i + 1}: {(MessageWithInteger)message.Body}");
+                channel.Send(message);
+            }
+        }
+
+        private static void ReceiveMessageFromSubsystemB(MessageQueue channel)
+        {
+            channel.Formatter = new XmlMessageFormatter(new Type[] { typeof(Int32) });
+            channel.ReceiveCompleted += (object source, ReceiveCompletedEventArgs asyncResult) =>
+            {
+                MessageQueue messageQueue = (MessageQueue)source;
+                var message = messageQueue.EndReceive(asyncResult.AsyncResult);
+                var sum = (Int32)message.Body;
+                Console.WriteLine($"Received from B: Sum of message {message.CorrelationId} is {sum}");
+                messageQueue.BeginReceive();
+            };
+            channel.BeginReceive();
         }
     }
 }
